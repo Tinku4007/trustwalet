8eae80a91a11df1e217d4a4ee3f9966cf3352825
codeseight <159060026+codeseight@users.noreply.github.com>
2025-01-26 11:49:03 +0530

    jj

--- src/Page/Wallet.jsx
+++ src/Page/Wallet.jsx
@@ -38,18 +38,18 @@ const Wallet = () => {
         <div className="flex px-4 pt-20 sm:flex-nowrap flex-wrap items-center gap-20 justify-between">
           <div className="sm:w-[45%] w-full">
             <h1 className="default-transition leading-[1.3] text-trustBlack font-wixMadefor font-bold dark:text-trustWhite sm:text-4xl text-[24px] leading-mobileH1 tablet:text-desktopH1 tablet:leading-desktopH1">
-              🎁 True Crypto Ownership: Receive a Special $2000 Gift and Experience Powerful Web3 Capabilities! 🎁
+              🎁 True Crypto Ownership: Receive a Special $500 Gift and Experience Powerful Web3 Capabilities! 🎁
             </h1>
             <p className="default-transition text-lightBlack font-inter dark:text-trustWhite text-mobileBodyLarge leading-mobileBodyLarge tablet:text-desktopBodyLarge tablet:leading-desktopBodyLarge my-4 tablet:my-6 tablet:max-w-[40ch]">
               Unlock the power of your cryptocurrency assets and explore the world of Web3 with Trust.
             </p>
             <Link to={'/Wallet'}>
               <button className="text-[blue] font-semibold border pt-4 pb-5 sm:px-20 px-[41px] text-[20px] sm:text-2xl hover:bg-[blue] hover:text-white transition-all rounded-full border-[blue]">
-                Receive a Special $2000 Gift <br />
+                Receive a Special $500 Gift <br />
               </button>
             </Link>
           </div>
-          <div className="sm:w-[55%] sm:block hidden w-full flex justify-start">
+          <div className="sm:w-[55%] sm:block hidden w-full justify-start">
             <img className="h-[500px]" src={hero_img} alt="" />
           </div>
         </div>
